5ff0cba8c7470197247dd82eb5500365dfbc2722
Saeed Rasooli <saeed.gnu@gmail.com>
2024-03-24 04:48:48 +0330

    fix ruff errors

--- number2word.py
+++ number2word.py
@@ -2,11 +2,11 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-import sys
 import locale
+import sys
 
 
-class number2word(object):
+class number2word:
     def __init__(self, number):
         self.number = number
 
@@ -28,48 +28,117 @@ class number2word(object):
         for i in range(len(array_number)):
             place = len(array_number) - i
             returnmsg = returnmsg + self.convert(array_number[i], place)
-            # if (array_number[i+1]) and (array_number[i+1] > 0):
-            if (0 <= i) and (i < len(array_number) - 1):
+            # if array_number[i+1] and array_number[i+1] > 0:
+            if 0 <= i < len(array_number) - 1:
                 returnmsg = returnmsg + " و "
         print(returnmsg)
 
-    def convert_number(self, number):
+    @staticmethod
+    def convert_number(number):
         locale.setlocale(locale.LC_ALL, "")
-        x = self.number
+        x = number
         x = int(float(x))
         x1 = locale.format_string("%d", x, grouping=True)
         # frappe.throw(type(x).__name__)
         if x < 0 or x > 999999999999:
-            print("العدد خارج النطاق")
-        else:
-            return x1
+            raise ValueError("Value out of range")
+        return x1
 
-    def convert(self, number, place):
+    @staticmethod
+    def convert(number, place):
         # take in charge the sex of NUMBERED
         #  sex =self.sex
         returnmsg = ""
 
-        sex = "male"
+        # sex = "male"
         # // the number word in arabic for masculine and feminine
 
-        # words = list[{'male':list[{
-        # 	'0': '' ,'1': 'واحد' ,'2': 'اثنان' ,'3' : 'ثلاثة','4' : 'أربعة','5' : 'خمسة',
-        # 	'6' : 'ستة','7' : 'سبعة','8' : 'ثمانية','9' : 'تسعة','10' : 'عشرة',
-        # 	'11' : 'أحد عشر','12' : 'اثنا عشر','13' : 'ثلاثة عشر','14' : 'أربعة عشر','15' : 'خمسة عشر',
-        # 	'16' : 'ستة عشر','17' : 'سبعة عشر','18' : 'ثمانية عشر','19' : 'تسعة عشر','20' : 'عشرون',
-        # 	'30' : 'ثلاثون','40' : 'أربعون','50' : 'خمسون','60' : 'ستون','70' : 'سبعون',
-        # 	'80' : 'ثمانون','90' : 'تسعون', '100':'مئة', '200':'مئتان', '300':'ثلاثمئة', '400':'أربعمئة', '500':'خمسمئة',
-        # 	'600':'ستمئة', '700':'سبعمئة', '800':'ثمانمئة', '900':'تسعمئة'}],
-        # 'female':list[{
-        # 	'0': '' ,'1': 'واحدة' ,'2': 'اثنتان' ,'3' : 'ثلاث','4' : 'أربع','5' : 'خمس',
-        # 	'6' : 'ست','7' : 'سبع','8' : 'ثمان','9' : 'تسع','10' : 'عشر',
-        # 	'11' : 'إحدى عشرة','12' : 'ثنتا عشرة','13' : 'ثلاث عشرة','14' : 'أربع عشرة','15' : 'خمس عشرة',
-        # 	'16' : 'ست عشرة','17' : 'سبع عشرة','18' : 'ثمان عشرة','19' : 'تسع عشرة','20' : 'عشرون',
-        # 	'30' : 'ثلاثون','40' : 'أربعون','50' : 'خمسون','60' : 'ستون','70' : 'سبعون',
-        # 	'80' : 'ثمانون','90' : 'تسعون', '100':'مئة', '200':'مئتان', '300':'ثلاثمئة', '400':'أربعمئة', '500':'خمسمئة',
-        # 	'600':'ستمئة', '700':'سبعمئة', '800':'ثمانمئة', '900':'تسعمئة'
-        #     }]
-        #               }]
+        # words = list[
+        #     {
+        #         "male": list[
+        #             {
+        #                 "0": "",
+        #                 "1": "واحد",
+        #                 "2": "اثنان",
+        #                 "3": "ثلاثة",
+        #                 "4": "أربعة",
+        #                 "5": "خمسة",
+        #                 "6": "ستة",
+        #                 "7": "سبعة",
+        #                 "8": "ثمانية",
+        #                 "9": "تسعة",
+        #                 "10": "عشرة",
+        #                 "11": "أحد عشر",
+        #                 "12": "اثنا عشر",
+        #                 "13": "ثلاثة عشر",
+        #                 "14": "أربعة عشر",
+        #                 "15": "خمسة عشر",
+        #                 "16": "ستة عشر",
+        #                 "17": "سبعة عشر",
+        #                 "18": "ثمانية عشر",
+        #                 "19": "تسعة عشر",
+        #                 "20": "عشرون",
+        #                 "30": "ثلاثون",
+        #                 "40": "أربعون",
+        #                 "50": "خمسون",
+        #                 "60": "ستون",
+        #                 "70": "سبعون",
+        #                 "80": "ثمانون",
+        #                 "90": "تسعون",
+        #                 "100": "مئة",
+        #                 "200": "مئتان",
+        #                 "300": "ثلاثمئة",
+        #                 "400": "أربعمئة",
+        #                 "500": "خمسمئة",
+        #                 "600": "ستمئة",
+        #                 "700": "سبعمئة",
+        #                 "800": "ثمانمئة",
+        #                 "900": "تسعمئة",
+        #             }
+        #         ],
+        #         "female": list[
+        #             {
+        #                 "0": "",
+        #                 "1": "واحدة",
+        #                 "2": "اثنتان",
+        #                 "3": "ثلاث",
+        #                 "4": "أربع",
+        #                 "5": "خمس",
+        #                 "6": "ست",
+        #                 "7": "سبع",
+        #                 "8": "ثمان",
+        #                 "9": "تسع",
+        #                 "10": "عشر",
+        #                 "11": "إحدى عشرة",
+        #                 "12": "ثنتا عشرة",
+        #                 "13": "ثلاث عشرة",
+        #                 "14": "أربع عشرة",
+        #                 "15": "خمس عشرة",
+        #                 "16": "ست عشرة",
+        #                 "17": "سبع عشرة",
+        #                 "18": "ثمان عشرة",
+        #                 "19": "تسع عشرة",
+        #                 "20": "عشرون",
+        #                 "30": "ثلاثون",
+        #                 "40": "أربعون",
+        #                 "50": "خمسون",
+        #                 "60": "ستون",
+        #                 "70": "سبعون",
+        #                 "80": "ثمانون",
+        #                 "90": "تسعون",
+        #                 "100": "مئة",
+        #                 "200": "مئتان",
+        #                 "300": "ثلاثمئة",
+        #                 "400": "أربعمئة",
+        #                 "500": "خمسمئة",
+        #                 "600": "ستمئة",
+        #                 "700": "سبعمئة",
+        #                 "800": "ثمانمئة",
+        #                 "900": "تسعمئة",
+        #             }
+        #         ],
+        #     }
+        # ]
         words = {
             "male": {
                 "0": "",
@@ -159,21 +228,19 @@ class number2word(object):
         mf = {"1": "male", "2": "male", "3": "male", "4": "male"}
         number_length = len(str(number))
 
-        # we are dealing with 3 digits number in each loop the main method calls convert methode
-        # and pass a string with tree digit in it ... for example 123 or 19 or 3 ..
+        # we are dealing with 3 digits number in each loop the main method calls convert
+        # method and pass a string with tree digit in it ...
+        # for example 123 or 19 or 3 ..
 
         # we will clean left zero for example 001 will be 1 ,,
         # 012 will be 12
 
         if int(number) == 0:
             return ""
-        elif number[0] == 0:
+        if number[0] == 0:
             if number[1] == 0:
-                number = int(number[:-1])
-                return number
-            else:
-                number = int(number[:-2])
-                return number
+                return int(number[:-1])
+            return int(number[:-2])
 
         # switching number length
         # if number have on digits like "1"
@@ -246,70 +313,69 @@ class number2word(object):
 
                 return returnmsg
 
+            threey = int(number[0]) * 100
+            threey = str(threey)
+            if words[mf[str(place)]][threey]:
+                returnmsg = returnmsg + words[mf[str(place)]][threey]
+
+            twoyony = (int(number[1]) * 10) + int(number[2])
+            if int(twoyony) == 2:
+                if place == 1:
+                    twoyony = words[mf[str(place)]]["2"]
+                if place == 2:
+                    twoyony = " ألفان"
+                if place == 3:
+                    twoyony = " مليونان"
+                if place == 4:
+                    twoyony = " ملياران"
+
+                if int(threey) != 0:
+                    twoyony = "و" + str(twoyony)
+
+                returnmsg = returnmsg + " " + twoyony
+
+            elif int(twoyony) == 1:
+                twoyony = str(twoyony)
+                if place == 1:
+                    twoyony = words[mf[str(place)]]["1"]
+                if place == 2:
+                    twoyony = "ألف"
+                if place == 3:
+                    twoyony = "مليون"
+                if place == 4:
+                    twoyony = "مليار"
+
+                if int(threey) != 0:
+                    twoyony = "و" + str(twoyony)
+
+                returnmsg = returnmsg + " " + twoyony
+
             else:
-                threey = int(number[0]) * 100
-                threey = str(threey)
-                if words[mf[str(place)]][threey]:
-                    returnmsg = returnmsg + words[mf[str(place)]][threey]
-
-                twoyony = (int(number[1]) * 10) + int(number[2])
-                if int(twoyony) == 2:
-                    if place == 1:
-                        twoyony = words[mf[str(place)]]["2"]
-                    if place == 2:
-                        twoyony = " ألفان"
-                    if place == 3:
-                        twoyony = " مليونان"
-                    if place == 4:
-                        twoyony = " ملياران"
-
-                    if int(threey) != 0:
-                        twoyony = "و" + str(twoyony)
-
-                    returnmsg = returnmsg + " " + twoyony
-
-                elif int(twoyony) == 1:
-                    twoyony = str(twoyony)
-                    if place == 1:
-                        twoyony = words[mf[str(place)]]["1"]
-                    if place == 2:
-                        twoyony = "ألف"
-                    if place == 3:
-                        twoyony = "مليون"
-                    if place == 4:
-                        twoyony = "مليار"
-
-                    if int(threey) != 0:
-                        twoyony = "و" + str(twoyony)
-
-                    returnmsg = returnmsg + " " + twoyony
-
+                # if ((words[mf[str(place)]][twoyony])):
+                twoyony = str(twoyony)
+                if twoyony in words[mf[str(place)]]:
+                    # if words.has_key(twoyony):
+                    twoyony = words[mf[str(place)]][twoyony]
                 else:
-                    # if ((words[mf[str(place)]][twoyony])):
-                    twoyony = str(twoyony)
-                    if twoyony in words[mf[str(place)]]:
-                        # if words.has_key(twoyony):
-                        twoyony = words[mf[str(place)]][twoyony]
-                    else:
-                        twoy = int(number[1]) * 10
-                        twoy = str(twoy)
-                        ony = number[2]
-                        twoyony = (
-                            words[mf[str(place)]][ony]
-                            + " و"
-                            + words[mf[str(place)]][twoy]
-                        )
-                    if (twoyony != "") and (int(threey) != 0):
-                        returnmsg = returnmsg + " و" + twoyony
-                    else:
-                        returnmsg = returnmsg + " " + twoyony
+                    twoy = int(number[1]) * 10
+                    twoy = str(twoy)
+                    ony = number[2]
+                    twoyony = (
+                        words[mf[str(place)]][ony]
+                        + " و"
+                        + words[mf[str(place)]][twoy]
+                    )
+                if twoyony and int(threey) != 0:
+                    returnmsg = returnmsg + " و" + twoyony
+                else:
+                    returnmsg = returnmsg + " " + twoyony
 
-                    if place == 2:
-                        returnmsg = returnmsg + " ألف"
-                    if place == 3:
-                        returnmsg = returnmsg + " مليون"
-                    if place == 4:
-                        returnmsg = returnmsg + " مليار"
+                if place == 2:
+                    returnmsg = returnmsg + " ألف"
+                if place == 3:
+                    returnmsg = returnmsg + " مليون"
+                if place == 4:
+                    returnmsg = returnmsg + " مليار"
 
         return returnmsg
 
